8b7136445236eaf5acfc8d6c0c5ade9f787b7926
Ben Brown <ben.raymond.brown@gmail.com>
2015-11-20 21:17:44 -0500

    Add vector math tests, standardize function names
    Performs tests on randomly generated vectors for correctness of known
    relations. Mutators are renamed to contain "Me", ie "rotateMe",
    "incrementMe", etc. Functions are reordered such that "setters" are
    adjacent to "getters".  Vector3d.js and its tests are linted.
    
    API:
    
    Added a short block for aliasing the functions with the old names.
    Added compatibility function wrapper for rotateAbout (just to keep
    things running and testable).  This calls rotateMe, which takes an
    object with rotation details: pivotPoint (location in space about
    which to rotate), axis: the axis of rotation (it can be thought of as
    passing through the pivot point), and the angle of rotation.
    
    Testing:
    
    Extends Math to include an approximate equals, Math.isClose(a,b).
    
    Vector3d tests are found in tests/Vector3dtests.js.
    
    tests/Vector3dtests.html runs the tests.
    
    The first tests are testAxes, testing if axes (unit vectors (1,0,0),
    (0,1,0), (0,0,1)) are perpendicular, length one, etc.
    
    Tested vector adding (subtracting) for commutativity
    (anti)-commutativity.

---
+++ tests/Vector3dTests.js
@@ -0,0 +1,240 @@
+Math.relativeTol = 1E-5;
+Math.absoluteTol = 1E-8;
+
+Math.isClose = function(a, b){
+	//Based on Numpy implementation: 
+	//http://docs.scipy.org/doc/numpy-dev/reference/generated/numpy.isclose.html
+	return (Math.abs(a-b) <= Math.absoluteTol + Math.relativeTol * Math.abs(b)); 
+};
+
+Vector3d.prototype.isClose = function(v){
+	return (Math.isClose(this.x, v.x) && Math.isClose(this.y, v.y) && Math.isClose(this.z, v.z));
+};
+
+
+Vector3d.prototype.tests = function(numSamples){
+/*	The usage of every test<Property> is that you expect that property to be "numerically true."
+	Failing the test logs to the console. Any output (other than all passed) should be considered a failure.
+*/	
+	var i, j;
+	var u, v;
+	var passed = true;
+	if(numSamples < 2) numSamples = 3;
+
+	function testLength(vector, expectedLength){
+		if(Math.isClose(vector.magnitude(), expectedLength)){
+			return true;
+		}else{
+			console.log("Expected length "+ expectedLength + " for vector " + vector.toString());
+			return false;
+		}
+	}
+
+	function testEquality(vector, expectedVector){
+		if (vector.isClose(expectedVector)){
+			return true;
+		}else if (testLength(vector, expectedVector.magnitude())){
+			console.log("Correct length, but wrong direction.");
+		}else{
+			console.log("Expected vector: " + expectedVector.toString());
+			console.log("Found vector: " + vector.toString());
+		}
+		return false;
+	}
+
+	function testPerpendicular(v1, v2){
+		if(Math.isClose(v1.dot(v2), 0)){
+			return true;
+		}else{
+			console.log("Failed perpendicularity: " + v1.toString() + ", " + v2.toString());
+			return false;
+		}
+	}
+
+	function testParallel(v1, v2){
+		var cosOpeningAngle = v1.dot(v2) / (v1.magnitude() * v2.magnitude());
+		if (testCoAxial(v1,v2)){
+			if (cosOpeningAngle > 0){
+				return true;
+			}else{
+				console.log("Vectors were anti-parallel.  (Did you want testCoAxial?)");
+				return false;
+			}			
+		}else{
+			return false;
+		}
+	}
+
+	function testCoAxial(v1, v2){
+		var cosOpeningAngle = v1.dot(v2) / (v1.magnitude() * v2.magnitude());
+		if(Math.isClose(Math.abs(cosOpeningAngle), 1)){
+			return true;
+		}else{
+			console.log("Failed to be parallel or anti-parallel: " + v1.toString() + ", " + v2.toString());
+			console.log("Opening angle): " + Math.acos(openingAngle));
+			return false;
+		}
+	}
+
+	function testDot(v1, v2){
+		if (Math.isClose(v1.dot(v2), v2.dot(v1))){
+			return true;
+		}else{
+			console.log("Dot product failed commutivitiy.");
+			return false;
+		}			
+	}
+
+	function testCross(v1, v2){
+		var v3 = v1.cross(v2);
+		if (!testEquality(v3, v2.cross(v1).scaleMe(-1))){
+			console.log("Cross product failed anti-commutivity.");
+			return false;
+		}
+		if (!(testPerpendicular(v3, v1) && testPerpendicular(v3, v2))){
+			console.log("Cross product failed perpendicularity.");
+			return false;
+		}
+		return true;
+	}
+
+	function testAxes(){
+	//Test known relationships with specific vectors.
+		var xAxis = new Vector3d(1, 0, 0);
+		var yAxis = new Vector3d(0, 1, 0);
+		var zAxis = new Vector3d(0, 0, 1);
+		var passed = true;
+		axes = [xAxis, yAxis, zAxis];
+		for(i=0; i < axes.length; i++){
+			passed &= testLength(axes[i], 1);
+			passed &= testParallel(axes[i], axes[i]);
+			passed &= testEquality(axes[i].cross(axes[(i+1)%3]), axes[(i+2)%3]);
+			for(j=0; j < i; j++){
+				passed &= testPerpendicular(axes[i], axes[j]);
+			}
+		}
+
+		xAxis.original = xAxis.copy();
+		passed &= testEquality(xAxis.rotateMe({axis: zAxis, angle: Math.PI/2}), yAxis);
+		passed &= testEquality(xAxis.rotateMe({axis: xAxis.original, angle:Math.PI/2}), zAxis);
+		passed &= testEquality(xAxis.rotateMe({axis: yAxis, angle:Math.PI/2}), xAxis.original);	
+		return passed;
+	}
+
+	passed &= testAxes();
+
+	var unitVectors = [];
+	var vectors = [];
+
+	for(i=0; i < numSamples; i++){
+		u = Vector3d.prototype.unitRandom();
+		u.original = u.copy();
+		unitVectors.push(u);
+
+		var x = Math.random();// 0 <= x < 1
+		x = x / (1-x);// 0 <= x < Infinity
+		v = Vector3d.prototype.randomOfMagnitude(x);
+		v.expectedLength = x;
+		v.original = v.copy();
+		vectors.push(v);
+	}
+
+	for(i=0; i < unitVectors.length; i++){
+		passed &= testLength(unitVectors[i], 1);
+	}
+
+	for (i=0; i < vectors.length; i++){
+		passed &= testLength(vectors[i], vectors[i].expectedLength);
+		passed &= testEquality(vectors[i].cross(vectors[i]), new Vector3d(0,0,0));
+	}
+
+	for (i=0; i < vectors.length; i++){
+		for (j = 0; j < i; j++){
+			var diff = vectors[i].copy().decrementMe(vectors[j]);
+			var sum  = vectors[i].copy().incrementMe(vectors[j]);
+			if (!testEquality(diff, vectors[j].copy().decrementMe(vectors[i]).scaleMe(-1))){
+				console.log("Vector subtraction failed anti-commutivity.");
+				passed = false;
+			}
+			if (!testEquality(sum,  vectors[j].copy().incrementMe(vectors[i]))){
+				console.log("Vector sum failed commutivity.");
+				passed = false;
+			}
+			passed &= testCross(vectors[i], vectors[j]);
+			passed &= testDot(vectors[i], vectors[j]);
+		}
+	}	
+
+	//Triple Product identities.
+	//If these fail, but the above pass, likely indicates API problems, not math problem.
+	for (i=0; i < numSamples; i++){
+		j = Math.floor(vectors.length * Math.random());
+		k = Math.floor(vectors.length * Math.random());
+		
+		var vi=vectors[i],vj=vectors[j],vk=vectors[k];
+
+		var tripleScalar = vi.dot(vj.cross(vk));
+		passed &= Math.isClose(tripleScalar, vj.dot(vk.cross(vi)));
+		passed &= Math.isClose(tripleScalar, vk.dot(vi.cross(vj)));
+
+		var tripleVector = vi.cross(vj.cross(vk));
+		tripleVector.incrementMe(vj.cross(vk.cross(vi)));
+		tripleVector.incrementMe(vk.cross(vi.cross(vj)));
+		passed &= testEquality(tripleVector, new Vector3d(0,0,0));
+	}
+
+	//Vector projections.
+	for (i=0; i < vectors.length; i++){
+		u = unitVectors[i];
+		v = unitVectors[i];
+		var vPlane = v.projectOntoPlane(u);
+		var vAxial = v.projectOntoAxis(u);
+		var vExpected = vPlane.copy().incrementMe(vAxial);
+		passed &= testEquality(v, vExpected);
+		passed &= testPerpendicular(vPlane, u);
+		passed &= testCoAxial(vAxial, u);
+	}
+
+	//Try some rotations:
+	for(i=0; i< vectors.length; i++){
+		v = vectors[i];
+		for(j=0; j < unitVectors.length; j++){
+			u = unitVectors[j];
+			v.rotateMe({axis:u, angle:0});
+			if(!testEquality(v, v.original)){
+				console.log("Rotation by zero failed to recover original vector.");
+				console.log("Original: "  + v.original.toString());
+				console.log("Recovered: " + v.toString());
+				passed = false;
+			}
+
+			var rotationAngle = 2 * Math.PI * Math.random();
+			v.rotateMe({axis:u, angle:rotationAngle});
+
+			if (! Math.isClose(v.openingAngle(u), v.original.openingAngle(u))){
+				console.log("Rotation failed to preserve opening angle between " + u.toString() + "and " + v.toString());
+				passed = false;
+			}
+
+			v.rotateMe({axis:u, angle:-rotationAngle});
+			if(!testEquality(v, v.original)){
+				console.log("Rotation followed by inverse rotation failed to recover original vector.");
+				console.log("Original: " + v.original.toString());
+				console.log("Recovered: " + v.toString());
+				passed = false;
+			}
+		}
+	}
+	return passed;
+};
+
+Vector3d.prototype.openingAngle = Vector3d.prototype.openingAngle_LawCosines;
+
+numSamples = 1000;
+//numSamples = 10000; This causes the page to become unresponsive, profiling hangs in chrome every time.
+if (Vector3d.prototype.tests(numSamples)){ 
+	console.log("All Vector3d tests passed with sample size "+ numSamples +".");
+}else{
+	console.log("Some Vector3d tests failed.");
+}
+
--- vector3d.js
+++ vector3d.js
@@ -1,13 +1,40 @@
+Math.TAU = 2 * Math.PI;
+
 function Vector3d(x, y, z){
 	this.x = x;
 	this.y = y;
 	this.z = z;
 }
 
+Vector3d.prototype.copy = function(v){
+	return new Vector3d(this.x, this.y, this.z);
+};
+
 Vector3d.prototype.setXYZ = function(x, y, z){
 	this.x = x;
 	this.y = y;
 	this.z = z;
+};
+
+Vector3d.prototype.asXYZ = function(){
+	return { x: this.x,
+			y: this.y,
+			z: this.z};
+};
+
+Vector3d.prototype.fromRThetaPhi = function(r, theta, phi){
+	return new Vector3d(r*Math.sin(theta)*Math.cos(phi), r*Math.sin(theta)*Math.sin(phi), r*Math.cos(theta));
+};
+
+Vector3d.prototype.setRThetaPhi = function (r, theta, phi){
+	this.x = r*sin(theta)*cos(phi);
+	this.y = r*sin(theta)*sin(phi);
+	this.z = r*cos(theta);
+};
+
+Vector3d.prototype.asRThetaPhi = function(){
+	var u = this.unitVector();
+	return {r:this.magnitude(), theta:Math.acos(v.z), phi:Math.atan2(v.y, v.x)};
 };
 
 Vector3d.prototype.setFromV = function(v){
@@ -16,18 +43,13 @@ Vector3d.prototype.setFromV = function(v){
 	this.z = v.z;
 };
 
-Vector3d.prototype.asXYZ = function(){
-	return { x: this.x,
-			y: this.y,
-			z: this.z}
+Vector3d.prototype.zeroMe = function(){
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
 };
 
-Vector3d.prototype.zero = function(){
-    this.setXYZ(0., 0., 0.);
-}
-
-
-Vector3d.prototype.sumsq = function(){
+Vector3d.prototype.sumSquares = function(){
 	//The cheapest "size" measure of a vector.
 	return  this.x*this.x +
 			this.y*this.y +
@@ -35,120 +57,22 @@ Vector3d.prototype.sumsq = function(){
 };
 
 Vector3d.prototype.magnitude = function(){
-	return Math.sqrt(this.sumsq());
-}
-
-
-Vector3d.prototype.dot = function(v){
-	return  this.x*v.x + 
-			this.y*v.y + 
-			this.z*v.z;
+	return Math.sqrt(this.sumSquares());
 };
 
-Vector3d.prototype.dist_squared = function(v){
-	var dx, dy, dz;
-	dx = this.x - v.x;
-	dy = this.y - v.y;
-	dz = this.z - v.z;
-	return dx*dx + dy*dy + dz*dz;
-};
+Vector3d.prototype.theta = function(){
+	//Theta meaured from north pole.
+	// This method avoids a Math.sqrt() call.
 
-Vector3d.prototype.distance = function(v){
-	return Math.sqrt(this.dist_squared(v));
-};
-
-Vector3d.prototype.dist_cubed = function(v){
-	var d = this.distance(v);
-	return d*d*d;
-};
-
-Vector3d.prototype.rOverRCubed_in_place = function(){
-	//Takes a vector and _overwrites_ it with r_vector/r^3
-	//This is exactly the gravity vector dependence.
-	var r_sq = this.sumsq();
-	var r_cubed = r_sq * Math.sqrt(r_sq);
-	this.x /= r_cubed;
-	this.y /= r_cubed;
-	this.z /= r_cubed;
-}
-
-Vector3d.prototype.FasterSqrt = function(xsq){
-	//No Error Checking whatsoever.  Also, not optimized yet, so do not expect actual speed yet.
-	var Numbuffer = new ArrayBuffer(4); //Bytes; float's typical size.
-	var f32View = new Float32Array(Numbuffer);
-	var bitsView = new Uint32Array(Numbuffer);
-	var one = new ArrayBuffer(4);
-	var float32_one = new Float32Array(one);
-	var bits_one = new Uint32Array(one);
-    
-    float32_one[0] = 1.;
-
-	f32View[0] = xsq;
-	bitsView[0] = (bitsView[0] + bits_one[0]) / 2;
-
-	x = f32View[0];
-	x = (x+xsq/x) / 2.
-	return (x+xsq/x)/2;
-}
-
-
-Vector3d.prototype.increment = function(v){
-	this.x += v.x;
-	this.y += v.y;
-	this.z += v.z;
-};
-
-Vector3d.prototype.decrement = function(v){
-	this.x -= v.x;
-	this.y -= v.y;
-	this.z -= v.z;
-};
-
-Vector3d.prototype.scale = function(a){
-	this.x *= a;
-	this.y *= a;
-	this.z *= a;
-};
-
-Vector3d.prototype.unitRandom = function(){
-	// Randomly directed uniformly over sphere.
-	var costheta = 2 * Math.random() - 1.;
-	var sintheta = Math.sqrt(1. - costheta*costheta);
-	var phi = 2*Math.PI*Math.random();
-	return new Vector3d(Math.cos(phi)*sintheta, Math.sin(phi)*sintheta, costheta);
-};
-
-Vector3d.prototype.randomOfMagnitude = function(magnitude){
-	//Randomly directed, but fixed magnitude:
-	var u = Vector3d.prototype.unitRandom();
-	u.scale(magnitude);
-	return u;
-};
-
-
-Vector3d.prototype.unitFromAngles = function(theta, phi){
-	// Phi is the angle in the x-y plane (called azimuth, like longitude)
-	// theta is the angle from the north celestial pole (like latitude, but starts at 0 at pole)
-	costheta = Math.cos(theta);
-	sintheta = Math.sin(theta);
-	cosphi = Math.cos(phi);
-	sinphi = Math.sin(phi);
-	u = new Vector3d(cosphi*sintheta, sinphi*sintheta,costheta);
-	return u;
-};
-
-Vector3d.prototype.unitVector = function(){
-	var u =Vector3d(this.x, this.y, this.z);
-	u.scale(1. / this.magnitude());
-	return u;
-};
-
-Vector3d.prototype.angles = function(){
-	//Should I set these and carry them around?... No?
-	v = this.unitVector();
-	theta = Math.acos(v.z);
-	phi   = Math.atan2(v.y, v.x);
-	return [theta, phi];
+	if (this.z === 0) {
+		return Math.PI / 2;
+	}else{
+		var sgn = this.z / abs(this.z);
+		//Since cos(x)**2 = 1/2 (cos(2 x)+1)
+		var cos_sq_theta = this.z*this.z/ this.sumSquares();
+		var abstheta = Math.acos(2 * cos_sq_theta - 1) / 2;
+		return sgn * abstheta;
+	}
 };
 
 Vector3d.prototype.phi = function(){
@@ -157,20 +81,131 @@ Vector3d.prototype.phi = function(){
 	return Math.atan2(this.y, this.x);
 };
 
-Vector3d.prototype.theta = function(){
-	//Theta meaured from north pole.
-	// This method avoids a Math.sqrt() call.
-	
-	if (this.z == 0) {
-		return Math.PI / 2.;
+Vector3d.prototype.dot = function(v){
+	return  this.x*v.x +
+			this.y*v.y +
+			this.z*v.z;
+};
+
+Vector3d.prototype.distSquared = function(v){
+	var dx, dy, dz;
+	dx = this.x - v.x;
+	dy = this.y - v.y;
+	dz = this.z - v.z;
+	return dx*dx + dy*dy + dz*dz;
+};
+
+Vector3d.prototype.distance = function(v){
+	return Math.sqrt(this.distSquared(v));
+};
+
+Vector3d.prototype.distCubed = function(v){
+	var d = this.distance(v);
+	return d*d*d;
+};
+
+Vector3d.prototype.rOverRCubed_in_place = function(){
+	//Takes a vector and _overwrites_ it with r_vector/r^3
+	//This is exactly the gravity vector dependence.
+	var rSq = this.sumSquares();
+	var rCubed = rSq * Math.sqrt(rSq);
+	this.x /= rCubed;
+	this.y /= rCubed;
+	this.z /= rCubed;
+};
+
+Vector3d.prototype.incrementMe = function(v){
+	this.x += v.x;
+	this.y += v.y;
+	this.z += v.z;
+	return this;
+};
+
+Vector3d.prototype.decrementMe = function(v){
+	this.x -= v.x;
+	this.y -= v.y;
+	this.z -= v.z;
+	return this;
+};
+
+Vector3d.prototype.scaleMe = function(a){
+	this.x *= a;
+	this.y *= a;
+	this.z *= a;
+	return this;
+};
+
+Vector3d.prototype.unitRandom = function(){
+	// Randomly directed uniformly over sphere.
+	var costheta = 2 * Math.random() - 1;
+	var sintheta = Math.sqrt(1 - costheta*costheta);
+	var phi = Math.TAU * Math.random();
+	return new Vector3d(Math.cos(phi)*sintheta, Math.sin(phi)*sintheta, costheta);
+};
+
+Vector3d.prototype.randomOfMagnitude = function(magnitude){
+	//Randomly directed, but fixed magnitude:
+	var u = Vector3d.prototype.unitRandom();
+	return u.scaleMe(magnitude);
+};
+
+Vector3d.prototype.unitFromAngles = function(theta, phi){
+	// Phi is the angle in the x-y plane (called azimuth, like longitude)
+	// theta is the angle from the north celestial pole (like latitude, but starts at 0 at pole)
+	return Vector3d.prototype.fromRThetaPhi(1, theta, phi);
+};
+
+Vector3d.prototype.unitMe = function(){
+	return this.scale(1 / this.magnitude());
+};
+
+Vector3d.prototype.unitVector = function(){
+	return this.copy().scale(1 / this.magnitude());
+};
+
+Vector3d.prototype.openingAngle_UnitDot = function(v2){
+	var u1 = this.unitVector();
+	var u2 = v2.unitVector();
+
+	return Math.acos(u1.dot(u2));
+};
+
+Vector3d.prototype.openingAngle_UnitLawOfCosines = function(v2){
+	var u1 = this.unitVector();
+	var u2 = v2.unitVector();
+	var oppositeSide = u1.decrementMe(u2);
+	return Math.acos(1 - oppositeSide.sumSquares()/2);
+};
+
+Vector3d.prototype.openingAngle_DotOnly = function(v2){
+	//Opening Angle refers to the angle between two vectors placed tail to tail.
+	var v1DotV2 = this.dot(v2);
+	if(v1DotV2 === 0){
+		return Math.PI/2;
 	}else{
-		var sgn = this.z / abs(this.z);
-		//Since cos(x)**2 = 1/2 (cos(2 x)+1)
-		cos_sq_theta = this.z*this.z/ this.sumsq();
-		abstheta = Math.acos(2. * cos_sq_theta - 1.) / 2.;
-		return sgn * abstheta;
+		v1DotV2 /= (v2.magnitude() * this.magnitude());
+		if (Math.abs(v1DotV2) > 1){
+			if(v1DotV2 > 1){
+				return 0;
+			}else{
+				return Math.PI;
+			}
+		}else{
+		  return Math.acos(v1DotV2);
+		}
 	}
 };
+
+Vector3d.prototype.openingAngle_LawCosines = function(v2){
+	//Opening Angle refers to the angle between two vectors placed tail to tail.
+	var oppositeSide = this.copy().decrementMe(v2);
+	var length1 = this.magnitude();
+	var length2 = v2.magnitude();
+	var costheta = (oppositeSide.sumSquares() - this.sumSquares() - v2.sumSquares())/(2*length1*length2);
+	return Math.acos(costheta);
+};
+
+Vector3d.prototype.openingAngle = Vector3d.prototype.openingAngle_UnitLawOfCosines;
 
 Vector3d.prototype.cross = function(v2){
 	v1Xv2 = new Vector3d(this.y*v2.z - this.z*v2.y,
@@ -179,33 +214,53 @@ Vector3d.prototype.cross = function(v2){
 	return v1Xv2;
 };
 
+Vector3d.prototype.rotateMe = function(rotation){
+	if (rotation.pivotPoint) this.decrementMe(rotation.pivotPoint);
 
-Vector3d.prototype.rotateAbout = function(axis, alpha){
-	//Assumes axis is a unit vector perpendicular to the plane of rotation
-    var vAxisOfRotation = this.projectAxis(axis);
+	var uRotationAxis = rotation.axis.unitVector();
 
-    var vPlaneOfRotation  = this.projectPlane(axis);
-    vPlaneOfRotation.scale(Math.cos(alpha));
+    var vAxial = this.projectOntoAxis(uRotationAxis);
 
-    var vCounterclockwise = axis.cross(this);
-    vCounterclockwise.scale(Math.sin(alpha));
+    var vPlaneParallel = this.projectOntoPlane(uRotationAxis);
+    vPlaneParallel.scaleMe(Math.cos(rotation.angle));
 
-    this.setFromV(vAxisOfRotation);
-    this.increment(vPlaneOfRotation);
-    this.increment(vCounterclockwise);
-}
+    var vPlanePerp = uRotationAxis.cross(this);
+    vPlanePerp.scaleMe(Math.sin(rotation.angle));
 
-Vector3d.prototype.projectAxis = function(axis){
-	var l = this.dot(axis);
-	var projectedV = new Vector3d(l * axis.x, l * axis.y, l * axis.z);
-	return projectedV;
-}
+    this.setFromV(vAxial);
+    this.incrementMe(vPlaneParallel);
+    this.incrementMe(vPlanePerp);
 
+    if (rotation.pivotPoint) this.incrementMe(rotation.pivotPoint);
+    return this;
+};
 
-Vector3d.prototype.projectPlane = function(plane){
+Vector3d.prototype.projectOntoAxis = function(axis){
+	return axis.copy().scaleMe(axis.dot(this));
+};
+
+Vector3d.prototype.projectOntoPlane = function(plane){
 	//plane is a Vector3d perpendicular to that plane.
 	var projectedV = new Vector3d(this.x, this.y, this.z);
 	//Subtract off the part of the vector perpendicular to the plane:
-	projectedV.decrement(this.projectAxis(plane));
+	projectedV.decrementMe(this.projectOntoAxis(plane));
 	return projectedV;
 };
+
+Vector3d.prototype.toString = function(){
+	return ("Vector3d(" + this.x + ", " + this.y + ", " + this.z + ")");
+};
+
+//Compatibility with old API:
+Vector3d.prototype.scale = Vector3d.prototype.scaleMe;
+Vector3d.prototype.sumsq = Vector3d.prototype.sumSquares;
+Vector3d.prototype.zero = Vector3d.prototype.zeroMe;
+Vector3d.prototype.increment = Vector3d.prototype.incrementMe;
+Vector3d.prototype.decrement = Vector3d.prototype.decrementMe;
+Vector3d.prototype.dist_squared = Vector3d.prototype.distSquared;
+
+Vector3d.prototype.rotateAbout = function(axisOfRotation, angleOfRotation){
+	return this.rotateMe({pivotPoint:new Vector3d(0, 0, 0), axis:axisOfRotation, angle:angleOfRotation});
+};
+
+
